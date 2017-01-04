54d05db2ceb7aeac3fbe956f762494ab4f38b301
Khalid Mohamud <kmohamud1@outlook.com>
2017-01-03 20:08:00 -0800

    supposed to be class 01 branch

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 'use strict';
-
-
-function Content() {
+var projlist = document.getElementById('projlist');
+var listArray = [];
+function Content(projectName, link, about, dateFinished) {
   this.projectName = projectName;
-  this.dateFinished = dateFinished;
+  this.link = link;
   this.about = about;
+  this.dateFinished = dateFinished;
+  listArray.push(this);
 }
+
+var makeProjList = function() {
+  for(var i = 0; i < listArray.length; i++){
+    projlist.innerHTML = '<li>'+listArray[i]+'</li>'
+  }
+}
+
+new Content('mobile first', '', 'making a website more phone friendly', '1221313');
+makeProjList();
