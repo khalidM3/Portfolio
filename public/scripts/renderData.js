38c1654b5a5b002e87c50b36707bcf5b943fb5c9
Khalid Mohamud <kmohamud1@outlook.com>
2017-01-21 11:56:45 -0800

    applied functional programming concepts

--- public/scripts/renderData.js
+++ public/scripts/renderData.js
@@ -1,40 +1,49 @@
 'use strict';
-var projects = [];
-function PopulateProjects(data) {
-  for(var key in data)
-    this[key] = data[key];
-}
-
-PopulateProjects.prototype.toHtml = function() {
-  // Grab the template script
-  var source = $('#project-template').html();
-  // Compile the template
-  console.log(source);
-  var comptemplate = Handlebars.compile(source);
-  return comptemplate(this);
-};
-
-PopulateProjects.loadAll = function (projData) {
-  projData.forEach(function(element){
-    projects.push(new PopulateProjects(element))
-  });
-}
-PopulateProjects.makeIndexPage = function() {
-  projects.forEach(function(element){
-    $('#eG').append(element.toHtml());
-})
-};
-
-PopulateProjects.fetchAll = function() {
-  if(localStorage.projData) {
-    var info = JSON.parse(localStorage.projData);
-    PopulateProjects.loadAll(info)
-    PopulateProjects.makeIndexPage();
-  } else {
-    var getinfo = $.getJSON('data/content.json', function(data) {
-      localStorage.setItem('projData', JSON.stringify(data));
-      PopulateProjects.loadAll(data);
-      PopulateProjects.makeIndexPage();
-    })
+(function(module){
+  function Projects(data) {
+    Object.keys(data).forEach(e => this[e] = data[e]);
   }
-}
+
+  Projects.all = []
+
+
+  Projects.prototype.toHtml = function() {
+    // Grab the template script
+    var source = $('#project-template').html();
+    // Compile the template
+    console.log(source);
+    var comptemplate = Handlebars.compile(source);
+    return comptemplate(this);
+  };
+
+  Projects.loadAll = projData => {
+    Projects.all = projData.map(data => new Projects(data));
+    //                 NOTE
+    //can also be wtiten like this
+    // projData.forEach(function(element){
+    //   Projects.all.push(new Projects(element))
+    // });
+  }
+  Projects.makeIndexPage = () => {
+    Projects.all.forEach(element => {
+      $('#eG').append(element.toHtml());
+    })
+  };
+//                 NOTE
+// gets all the info to make the projects from local storage or the data
+  Projects.fetchAll = function() {
+    if(localStorage.projData) {
+      var info = JSON.parse(localStorage.projData);
+      Projects.loadAll(info)
+      Projects.makeIndexPage();
+    } else {
+      var getinfo = $.getJSON('data/content.json', function(data) {
+        localStorage.setItem('projData', JSON.stringify(data));
+        Projects.loadAll(data);
+        Projects.makeIndexPage();
+      })
+    }
+  }
+  module.Projects = Projects;
+
+})(window)
