34aadb9fd83d412ff584934a3fb16c556d539683
Khalid Mohamud <kmohamud1@outlook.com>
2017-01-21 18:03:25 -0800

    class 7 comit

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('index.html', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFIle('index.html', {root: './public'})
 });
+app.get('/about', function(req,res) {
+  res.sendFile('index.html', {root: './public'})
+})
 
 app.listen(PORT, function() {
   console.log('The server has started on port '+ PORT);
